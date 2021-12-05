fd4e2696dad32ef96f9f93bac36335e3efdfb485
buiatuan <devils2tuan@gmail.com>
2021-12-05 14:51:55 +0700

    update

---
+++ laikep.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	float a = 4000;
+	int n = 4;
+	float i = 0.08;
+	float f4;
+	
+	f4 = 4000*pow(1+0.08,4);
+	
+	printf("tong tien sau 4 nam la %f\n",f4);
+}
