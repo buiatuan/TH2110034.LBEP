483c7e7adc69245f2795c78c68fd0e06ba4536a1
buiatuan <devils2tuan@gmail.com>
2021-12-22 09:52:30 +0700

    Assignment9

---
+++ Assignment9/Bai3_4Assignment9.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+float Perimeter(float a,float b,float c){
+	float p=a+b+c;
+	return p;
+}
+float Acreage(float a,float b,float c){
+	float p=Perimeter(a,b,c)/2;
+	float s=sqrt(p*(p-a)*(p-b)*(p-c));
+	return s;
+	
+}
+int main(){
+	float a,b,c;
+	do{
+		printf("input a= ");
+		scanf("%f",&a);
+		printf("input b= ");
+		scanf("%f",&b);
+		printf("input c= ");
+		scanf("%f",&c);
+	}while(a>=b+c ||b>=a+c ||c>=a+b);
+	printf("perimeter of triangle: %f\n",Perimeter(a,b,c));
+	printf("Acreage of triangle: %f\n",Acreage(a,b,c));
+}
